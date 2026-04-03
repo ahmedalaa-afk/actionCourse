7cad822ea2edf0f96c0c756367bd176ef804e9a7
Ahmed Alaa <ahmed.alaaeldine21@gmail.com>
2026-04-03 16:58:55 +0200

    Fix require path in test script to use correct module location

--- src/test.sh
+++ src/test.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
 EXPECTED="Hello, World!"
-OUTPUT=$(node -e "console.log(require('./app')('Test'))")
+OUTPUT=$(node -e "console.log(require('src/app')('Test'))")
 
 if [ "$OUTPUT" = "Hello, Test!" ]; then
     echo "Test passed!"
